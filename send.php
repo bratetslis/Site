95f0d0b43eeee783a366c793df59eb088f85c220
Nikolay Illichevski <ttiss@baraban.com>
2011-11-14 19:46:22 +0400

    blablabla

---
+++ send.php
@@ -0,0 +1,7 @@
+
+<?
+$to = "_@_";
+mail($to,"$sub",$mes);
+echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://www.svoisait.ru/form/_post.shtml'>Отправить ещё</a>";
+?>
+
