fe2fb7c36d6e07964ab76c5cfb1b9096b1dcd9a6
Nicholas Illychevsky <ttiss@baraban.com>
2011-11-15 11:29:18 +0400

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,5 @@
-
 <?
 $to = "pochta@bratetslis.ru";
 mail($to,"$sub",$mes);
-echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://bratetslis.ru/form/_post.shtml'>Отправить ещё</a>";
+echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://bratetslis.ru/contacts>Отправить ещё</a>";
 ?>
