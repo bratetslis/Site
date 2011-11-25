d6dfdcc74c7c2ef925c72f215cb6ffddc28fdfc9
Nicholas Illychevsky <ttiss@baraban.com>
2011-11-25 11:05:11 +0400

    Update send.php

--- send.php
+++ send.php
@@ -1,5 +1,71 @@
-<?
-$to = "pochta@bratetslis.ru";
-mail($to,"$sub",$mes);
-echo "Ваше сообщение с темой  $sub и текстом: $mes <br> отправлено<BR><center><a href='http://Bratetslis.ru/contacts'>Отправить еще</a>";
+
+<title>Сообщение отправлено</title>
+<?php
+/* Проверяем существуют ли переменные, которые передала форма обратной связи. 
+   Если не существуют, то мы их создаем.
+   Если форма передала пустые значения мы их удаляем */
+if (isset($_POST['fio'])) {$fio = $_POST['fio']; if ($fio == '') {unset($fio);}}
+if (isset($_POST['email'])) {$email = $_POST['email']; if ($email == '') {unset($email);}}
+if (isset($_POST['money'])) {$money = $_POST['money']; if ($money == '') {unset($money);}}
+if (isset($_POST['pr'])){$pr = $_POST['pr']; if ($pr == '') {unset($pr);}}
+if (isset($_POST['captcha'])){$captcha = $_POST['captcha'];}
+
+ 
+
+/* Проверяем заполнены ли все поля */
+if (isset($fio) && isset($email) && isset($money) && isset($pr))
+{
+
+/* Убираем все лишние пробелы, а также преобразуем все теги HTML в символы*/
+$fio = htmlspecialchars(trim($fio));
+$email = htmlspecialchars(trim($email));
+$money = htmlspecialchars(trim($money));
+
+/* Проверяем правильность ввода email-адреса */
+if(!preg_match("/[0-9a-z_]+@[0-9a-z_^\.]+\.[a-z]{2,3}/i", $email))
+{
+echo "<p>Неправильный формат e-mail адреса!</p>";
+}
+
+/* Проверяем правильность ввода капчи */
+  if ($captcha == $pr)
+  {
+/* Формируем сообщение */
+$address = "ttiss@baraban.com";
+$sub = "Сообщение с блога";
+$mes = "Автор назвался: $fio \nОставил такой E-mail: $email \n Текст сообщения: $money";
+
+/* Отправка сообщения */
+$verify = mail ($address,$sub,$mes,"Content-type:text/plain; charset = UTF-8\r\nFrom:$email");
+      if ($verify == 'true')
+    
+     {
+       echo "<body bgcolor='609df9'>
+<div style='margin-top: 30px'><table border='1' width='450' align='center' cellpadding='20' cellspacing='6' bgcolor='white'>
+<tr>
+<td>
+<div style='margin'><div align='center'>Ваше сообщение успешно отправлено! <p>Вернуться к моим друзьям <a href='http://bratetslis.ru/friends'><font size='+1'>Да</font></a></div>
+<img src='http://ваш_сайт/img/message.jpg'>
+<p><div align='center'>Я его прочитаю и отвечу вам!</div>
+<p><div align='right'><i><b>C уважением, ваш Братец Лис</b></i></div>
+</tr>
+</td>
+</table></div>";
+      }
+      else 
+    {
+	  echo "Сообщение не отправлено!";
+	  }
+  }
+  else
+  {
+  echo "Вы не правильно ввели сумму чисел с картинки";
+  }
+ 
+
+}
+else
+{
+echo "Вы заполнили не все поля!";
+}
 ?>
